eb371cda67d573c4338b90ccbda3b6719dee0001
Jordan Anderson <jordan.anderson8@icloud.com>
2021-01-11 21:18:04 -0800

    Added images and configured image sheet

--- main.lua
+++ main.lua
@@ -10,3 +10,39 @@ physics.setGravity(0,0)
 
 -- Seed the random number generator
 math.randomseed(os.time())
+
+-- Configure image sheet
+local sheetOptions = {
+    frames = {
+        { -- astroid 1
+            x = 0,
+            y = 0,
+            width = 102,
+            height = 85
+        },
+        { -- astroid 2
+            x = 0,
+            y = 85,
+            width = 90,
+            height = 83
+        },
+        { -- astroid 3
+            x = 0,
+            y = 168,
+            width = 100,
+            height = 97
+        },
+        { -- ship
+            x = 0,
+            y = 265,
+            width = 98,
+            height = 79
+        },
+        { -- laser
+            x = 98,
+            y = 265,
+            width = 14,
+            height = 40
+        },
+    },
+}
