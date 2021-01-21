2d7809c0b839c0c1a044015a06430735661eb207
Jordan Anderson <jordan.anderson8@icloud.com>
2021-01-20 21:45:24 -0700

    Beginning on a settings page

--- settings.lua
+++ settings.lua
@@ -1,5 +1,5 @@
 
-local composer = require("composer")
+local composer = require( "composer" )
 local display = require("display")
 local native = require("native")
 local audio = require("audio")
@@ -13,16 +13,8 @@ local scene = composer.newScene()
 
 local musicTrack
 
-local function goToGame()
-	composer.gotoScene("game", { time = 800, effect = "crossFade" })
-end
-
-local function goToHighScores()
-	composer.gotoScene("highscores", { time = 800, effect = "crossFade" })
-end
-
-local function goToSettings()
-	composer.gotoScene("settings", { time = 800, effect = "crossFade" })
+local function goToMenu()
+	composer.gotoScene("menu", { time = 800, effect = "crossFade" })
 end
 
 -- -----------------------------------------------------------------------------------
@@ -30,36 +22,21 @@ end
 -- -----------------------------------------------------------------------------------
 
 -- create()
-function scene:create(event)
-
+function scene:create( event )
 	-- Code here runs when the scene is first created but has not yet appeared on screen
 	local sceneGroup = self.view
-
 	-- Load the background
 	local background = display.newImageRect(sceneGroup, "background.png", 800, 1400)
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
-
-	-- Load the title
-	local title = display.newImageRect(sceneGroup, "title.png", 500, 80)
-	title.x = display.contentCenterX
-	title.y = 200
-
-	-- Load the buttons
-	local playButton = display.newText(sceneGroup, "Play", display.contentCenterX, 700, native.systemFont, 44)
-	playButton:setFillColor(0.80, 0.85, 1)
-	local highScoresButton = display.newText(sceneGroup, "High Scores", display.contentCenterX, 800, native.systemFont, 44)
-	highScoresButton:setFillColor(0.70, 0.75, 1)
-	local settingsButton = display.newText(sceneGroup, "Settings", display.contentCenterX, 900, native.systemFont, 44)
-	settingsButton:setFillColor(0.60, 0.65, 1)
-
-	-- Button handlers
-	playButton:addEventListener("tap", goToGame)
-	highScoresButton:addEventListener("tap", goToHighScores)
-	settingsButton:addEventListener("tap", goToSettings)
-
+	display.newText(sceneGroup, "Settings", display.contentCenterX, 100, native.systemFont, 44)
+	-- Menu button
+	local menuButton = display.newText(sceneGroup, "Menu", display.contentCenterX, 810, native.systemFont, 44)
+	menuButton:setFillColor(0.75,0.78,1)
+	menuButton:addEventListener("tap", goToMenu)
 	-- Background music
-	musicTrack = audio.loadSound("audio/Escape_Looping.wav")
+	musicTrack = audio.loadSound("audio/Midnight-Crawlers_Looping.wav")
+
 end
 
 
@@ -76,6 +53,7 @@ function scene:show( event )
 		-- Code here runs when the scene is entirely on screen
 		-- Start background music
 		audio.play(musicTrack, {channel=1, loops=-1})
+
 	end
 end
 
@@ -91,8 +69,8 @@ function scene:hide( event )
 
 	elseif ( phase == "did" ) then
 		-- Code here runs immediately after the scene goes entirely off screen
-		-- Remove the background music
 		audio.stop(1)
+		composer.removeScene("settings")
 
 	end
 end
